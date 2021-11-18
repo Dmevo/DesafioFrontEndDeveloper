115ba5c26318ded349ddcb5c499b4893525c4a21
Danilo Vasconcellos <danilodemv@hotmail.com>
2021-11-17 22:28:13 -0300

    Adicionando a funcionalidade AJAX

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -8,4 +8,12 @@ produto_lista = produto_dao.listar_todos()
 
 @app.route('/')
 def index():
-    return render_template("home.html", produto_lista=produto_lista[0:4])
+    return render_template("home.html", produto_lista=produto_lista)
+
+@app.route('/pag-1')
+def pag_1():
+    return render_template("produto.html", produto_lista=produto_lista[0:4])
+
+@app.route('/pag-2')
+def pag_2():
+    return render_template("produto.html", produto_lista=produto_lista[4:7])
